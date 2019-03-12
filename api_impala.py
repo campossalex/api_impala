f82baf0eee1c9b1f600257980b91f8fa092ea2a3
campossalex <32500181+campossalex@users.noreply.github.com>
2019-03-12 14:16:54 -0500

    Create api_impala.py

---
+++ api_impala.py
@@ -0,0 +1,11 @@
+from impala.dbapi import connect
+import json
+
+def query(args):
+    id = args.get('name')
+    conn = connect(host='10.0.0.168', port=21050)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * from simon_r.players where name = "' + id + '"')
+    for row in cursor:
+        return row
+ 
